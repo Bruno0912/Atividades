448281f742136a96d157f1b8a9df1f68bce571aa
Bruno0912 <106964693+Bruno0912@users.noreply.github.com>
2023-04-03 11:23:04 -0300

    Update Script03.js

--- Atividades/Script/Script03.js
+++ Atividades/Script/Script03.js
@@ -1,21 +1,21 @@
 function somarNumeros() {
-    // Inicializa a variável de soma com 0
+   
     var soma = 0;
-    // Inicializa a string de resultado com uma mensagem
+    
     var resultado = "A soma dos números de 1 a 1000 é: ";
-    // Loop pelos números de 1 a 1000 e soma cada um à variável de soma
+    
     for (var i = 1; i <= 1000; i++) {
         soma += i;
-        // Adiciona o número atual à string de resultado
+       
         resultado += i;
-        // Adiciona um sinal de adição entre os números, exceto para o último número
+
         if (i < 1000) {
             resultado += " + ";
         }
     }
-    // Adiciona o total à string de resultado
+   
     resultado += "Total: " + soma;
-    // Exibe a string de resultado na janela do navegador
-    //document.write(resultado);
+    
+  
     alert(resultado);
 }
